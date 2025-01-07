17fdc4ebefefa8849c25895e46239c8806940ce8
benedict <benedict1426paul@gmail.com>
2025-01-07 16:54:02 +0530

    updated resume

--- components/Social.jsx
+++ components/Social.jsx
@@ -7,7 +7,7 @@ const socials = [
   { icon: <FaLinkedinIn />, path: "https://www.linkedin.com/in/benedict-paul/", title: "LinkedIn" },
   { icon: <SiLeetcode />, path: "https://leetcode.com/u/benduBytes/", title: "LeetCode" },
   { icon: <FaGoogle />, path: "https://www.cloudskillsboost.google/public_profiles/01f3ceb8-968c-4361-b0b1-1a0701d7f4e7", title: "Google Cloud Skills Boost" },
-  { icon: <FaFile />, path: "https://drive.google.com/file/d/1XK98LvsP7hVqvLla5M_p6vdSlIpEfWJV/view?usp=sharing", title: "Resume" }
+  { icon: <FaFile />, path: "https://drive.google.com/file/d/1Ql49ybIfyPFkdoc7RFnShGtcmnD76Di_/view?usp=sharing", title: "Resume" }
 ];
 
 const Social = ({containerStyles, iconStyles }) => {
